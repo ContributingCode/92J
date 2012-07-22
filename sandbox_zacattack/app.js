a55f99760833c67b3f61833e54c1d4e05c6dbdad
ZacAttack <zacattackftw@gmail.com>
2012-07-22 11:22:40 -0700

    Added mongo to sandbox, and code to get facebook uid

--- sandbox_zacattack/app.js
+++ sandbox_zacattack/app.js
@@ -17,8 +17,42 @@ var app = express.createServer(
   })
 );
 
+// Create database instance
+if(process.env.VCAP_SERVICES)
+{
+	var env = JSON.parse(process.env.VCAP_SERVICES);
+	var mongo = env['mongodb-1.8'][0]['credentials'];
+}
+else{
+	var mongo = {
+		"hostname":"localhost",
+		"port":27017,
+		"username":"",
+		"password":"",
+		"name":"",
+		"db":"db"
+	}
+    }
+
+    var generate_mongo_url = function(obj){
+	obj.hostname = (obj.hostname || 'localhost');
+	obj.port = (obj.port || 27017);
+	obj.db = (obj.db || 'test');
+
+	if(obj.username && obj.password){
+		return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	}
+	else{
+		return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	}
+    }
+
+var mongourl = generate_mongo_url(mongo);
+
+
+
 // listen to the PORT given to us in the environment
- var port = (process.env.VMC_APP_PORT || 3000);
+var port = (process.env.VMC_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 var http = require('http');
 
@@ -59,10 +93,13 @@ function render_page(req, res) {
 }
 
 function handle_facebook_request(req, res) {
-
+ 
   // if the user is logged in
   if (req.facebook.token) {
 
+    req.facebook.get('/me', function(me) {
+    	console.log(me.id);
+    });
     async.parallel([
       function(cb) {
         // query 4 friends and send them to the socket for this socket id
