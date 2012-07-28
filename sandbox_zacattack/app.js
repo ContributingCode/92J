09504501fd7224b7c821348013e9b396b9543346
larry-fuy <larry.yong.fu@gmail.com>
2012-07-28 00:04:01 -0700

    add volunteermatch api

--- sandbox_zacattack/app.js
+++ sandbox_zacattack/app.js
@@ -1,6 +1,8 @@
 var async   = require('async');
 var express = require('express');
 var util    = require('util');
+var http = require('http');  
+
 //var jQuery = require('jQuery');
 
 
@@ -26,45 +28,60 @@ var app = express.createServer(
 
 // Connect app to Volunteer Match 
 // Stores account information for Volunteer Match
-//var volunteerMatch = {
-//  accountName: process.env.VOLUNTEER_MATCH_ACCOUNT,
-//  accountKey: process.env.VOLUNTEER_MATCH_KEY,
-//  accountSecret: process.env.VOLUNTEER_MATCH_SECRET
-//};
-/*
+var volunteerMatch = {
+  accountName: process.env.VOLUNTEER_MATCH_ACCOUNT,
+  accountKey: process.env.VOLUNTEER_MATCH_KEY
+};
+
 var name = volunteerMatch.accountName;
 var key = volunteerMatch.accountKey;
-var secret  = volunteerMatch.accountSecret;
-var path = ;
+var path = '/api/call';
 
-function SendRequest(action, query, type) {
-var timestamp = time();
-var nonce = hash('sha1', openssl_random_pseudo_bytes(20));
-var date = date('Y-m-d\TH:i:sO', timestamp);
-var digest = base64_encode(hash('sha256', $nonce . $date . self::$key, TRUE));
-var header_array = array( 'Content-Type' => 'application/json',
-					  'Authorization' => 'WSSE profile="UsernameToken"',
-					  'X-WSSE' => 
-					  'UsernameToken Username="' . self::$username . 
-					  '", PasswordDigest="' . $digest .
-					  '", Nonce="' . $nonce .
-					  '", Created="' . $date .
-					  '"');
-var url = path + "?action=" + action;
-if (query != NULL) {
-   var json_query = JSON.stringify(query);
-   url = url + "&query=" +  urlencode(json_query);
-}  
+// Send request to ask information
+function SendRequest(action, query) {
+	 if (query.length !=  0) {
+	 	 var url = path + '?action=' + action + '&key=' + key
+		  + '&query=' + JSON.stringify(query);
+	} else {
+	 	 var url = path + '?action=' + action + '&key=' + key;
+	}		  
+	console.log('url:, ' + url);
+	var get_options = {
+      	     host: 'www.volunteermatch.org',
+      	     path: url,
+  	 };	     
 
+
+       var get_req = http.get(get_options, function(res) {
+       console.log('STATUS: ' + res.statusCode);	      
+       console.log('HEADERS: ' + JSON.stringify(res.headers));      
+       res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+	  return chunk;
+      	});
+      });
+     
+     // for debugging
+      get_req.on('error', function(e) {
+      	      console.log("Got error: " + e.message);
+	});
+      console.log('host :' + get_options.host); 
+      return -1;
 }
 
-
-function DisplayResponse() {
-
+// input the location string
+function searchOrganizations(loc) {
+	 fd = ["name", "location", "title", "parentOrg", "vmUrl", "imageUrl"];
+	 conds = { location : loc,  nbOfResults : 20, pageNumber : 3, fieldsToDisplay : fd };
+	 return data = SendRequest('searchOrganizations', conds);
 }
 
-*/
-
+function searchOpportunities(loc) {
+	 fd = ["name", "location", "title", "parentOrg", "vmUrl", "imageUrl"];
+	 conds = { location : loc,  nbOfResults : 20, pageNumber : 3, fieldsToDisplay : fd };
+	 return data = SendRequest('searchOpportunities', conds);
+}
 
 // Create database instance
 if(process.env.VCAP_SERVICES)
