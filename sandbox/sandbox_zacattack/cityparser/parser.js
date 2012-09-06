3ce21296f0bdeda993952061c420efb90edeb141
ZacAttack <zacattackftw@gmail.com>
2012-09-06 12:18:05 -0700

    Finished Code that generates city database.  Upload of database backup to come

--- sandbox/sandbox_zacattack/cityparser/parser.js
+++ sandbox/sandbox_zacattack/cityparser/parser.js
@@ -1,24 +1,24 @@
 var request = require('request');
 
-// Database instance settings
+// Create database instance
 var mongo = {
-	"hostnmae": "localhost",
-	"port": 27017,
-	"username": "",
-	"password": "",
-	"name": "",
-	"db": "db"
+        "hostname": "localhost",
+        "port": 27017,
+        "username": "",
+        "password": "",
+        "name": "",
+        "db": "cities"
 }
 
 var params = {
-	host: mongo.hostname,
-	port: mongo.port,
-	username: mongo.username,
-	password: mongo.password,
-	db: mongo.db
+    host: mongo.hostname,
+    port: mongo.port,
+    username: mongo.username,
+    password: mongo.password,
+    db: mongo.db
 }
 
-// Initiate database connection
+// Initiate database connection.
 var collections = ["cities"];
 var db = require("mongojs").connect(params, collections);
 
@@ -32,26 +32,46 @@ db.cities.ensureIndex({ loc : "2d" }, function (err, log) {
   console.log('log: ' + log);	  
 });
 
+function isNumber(n) {
+	  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
-function insertCities(city)
+function insertCity(city)
 {
-    //TODO: Extract the name of the city (which can span multiple words)
-    var offset = 0;
-    var cityToInsert = {
-	    'USPS': city[0],
-	    'GEOID': city[1],
-	    'ANSICODE': city[2],
-	    'NAME': "",
+    if(!city){
+         console.log("End of Input")
+	 return;
+    }
+    var offset = 3;
+    var cityName = "";
+    while(!(isNumber(city[offset]))){
+	    console.log(offset);
+            if(!(isNumber(city[offset+1]))){
+		    cityName+=city[offset];
+	    }
+	    offset++;
+    }
+    offset--;
+
+    db.cities.save({
+            'USPS': city[0],
+            'GEOID': city[1],
+            'ANSICODE': city[2],
+            'NAME': cityName,
             'LSAD': city[offset],
-	    'FUNCSTAT': city[offset+1],
-	    'POP10': city[offset+2],
-	    'HU10': city[offset+3],
-	    'ALAND': city[offset+4],
-	    'AWATER': city[offset+5],
-	    'ALAND_SQMI': city[offset+6],
-	    'AWATER_SQMI': city[offset+7],
-	    'loc': [city[offset+8], city[offset+8]]
-    };
+            'FUNCSTAT': parseFloat(city[offset+1]),
+            'POP10': parseFloat(city[offset+3]),
+            'HU10': parseFloat(city[offset+4]),
+            'ALAND': parseFloat(city[offset+5]),
+            'AWATER': parseFloat(city[offset+6]),
+            'ALAND_SQMI': parseFloat(city[offset+7]),
+            'AWATER_SQMI': parseFloat(city[offset+8]),
+            'loc': [parseFloat(city[offset+9]), parseFloat(city[offset+10])]
+    }, function(err, log) {
+	    if(err)
+               console.log("Error " + err);
+	    console.log(log);
+    });
 }
 
 function getCities(url) {
@@ -64,14 +84,17 @@ function getCities(url) {
         }
 	// Get every line
 	var cities = body.split("\n");
-
+        console.log("We finished the request");
 	// First line Simply describes the fields, it is not relevant data.  Remove it.
 	cities.shift();
 
-	// Insert Each sity into the database.
+	// Insert Each city into the database.
 	cities.forEach(function(city) {
-	     insertCity(city[0].match(/[^ ]+/gi));
+	     console.log("loop iteration");
+	     insertCity(city.match(/[^ ]+/gi));
 	});
     });
 }
 
+//Go for it!
+getCities(censusDataUrl);
